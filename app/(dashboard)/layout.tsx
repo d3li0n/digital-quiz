501b5dde21dc2ca0626486c984de3decffd01ab9
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-03-31 12:26:40 -0700

    add mobile nav

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Link from "next/link"
 import { FaGithub } from "react-icons/fa6"
 
@@ -37,6 +38,48 @@ export default function QuizzesLayout({
                 </Link>
               </li>
             </ul>
+          </div>
+
+          <div className="sm:max-w-[264px] md:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <button className="block md:hidden focus:outline-none">
+                  <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                  </svg>
+                </button>
+              </SheetTrigger>
+              <SheetContent side="left" className="border-none bg-white">
+                <Link href="/" className="flex items-center gap-1">
+                  <p className="text-[26px] font-extrabold text-dark">Digital Quiz</p>
+                </Link>
+                <SheetClose asChild>
+                  <ul className="font-medium flex flex-col md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                    <li>
+                      <Link href="/quizzes" className="block p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent">
+                        Quizzes
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/quizzes/new" className="block p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent">
+                        Add Quiz
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/vote" className="block p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent">
+                        Vote
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="https://github.com/d3li0n/digital-quiz" className="block p-3 text-white bg-black rounded-lg">
+
+                        View on GitHub
+                      </Link>
+                    </li>
+                  </ul>
+                </SheetClose>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </nav>
--- app/(dashboard)/quizzes/page.tsx
+++ app/(dashboard)/quizzes/page.tsx
@@ -25,7 +25,7 @@ export default function Quizzes() {
   } else {
     return quizzes && (
       <>
-        <h2 className="text-2xl font-bold mb-12">Available Quizzes</h2>
+        <h2 className="text-2xl font-bold max-sm:mb-2 md:mb-12">Available Quizzes</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {quizzes.map((quiz: Quiz, index) => (
             <Card key={index} style={{ background: `linear-gradient(135deg, ${quiz.gradient})` }} className="text-white">
